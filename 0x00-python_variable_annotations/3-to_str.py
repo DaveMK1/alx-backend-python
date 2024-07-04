d59d2889bfcc9a6ebeb56a2508cdd205f54e70ae
David Agbesinyale <davidm7040@gmail.com>
2024-07-04 19:58:19 +0000

    Basic annotations - to string[

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+'''Contains a function that returns a string representation of a float
+'''
+
+
+def to_str(n: float) -> str:
+    '''Converts a floating-point number to a string.
+    '''
+    return str(n)
